b51593024eaac514b3ce558b2ef28cf954ee5bec
victorchiappetta <victorcchiappetta@gmail.com>
2026-05-27 13:00:58 -0300

    feat(exercicios-aula): resolve o exercício: contador de letras 'a' presentes em uma palavra

---
+++ exercicios_aula/010_contador_letra.py
@@ -0,0 +1,9 @@
+#%%
+palavra = "balalaika games"
+contador = 0
+for i in palavra:
+    if i == 'a':
+        contador += 1
+
+
+print(contador)
--- exercicios_aula/07_qtde_agua.py
+++ exercicios_aula/07_qtde_agua.py
@@ -1,7 +1,7 @@
 # Faça um programa que vende uma garrafa de água:
 # Se o cliente escolher água mineral natural, será cobrado R$1,50
 # Se o cliente escolher água mineral com gás, será cobrado R$2,50
-
+#%%
 texto = """
 Escolha a sua água para comprar
 (1) Água mineral natural - R$1.5
