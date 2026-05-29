f1f390fc8fc751dd185caa1b6e0aea77d882439a
victorchiappetta <victorcchiappetta@gmail.com>
2026-05-29 18:08:47 -0300

    docs(aula-dia-07): manipulação de arquivos, abertura, fechamento, leitura.
     utilizando 'open_file.read()', 'open_file.close()', 'whith open(nome_arquivo) as open_file'

--- dia_07/01_read.py
+++ dia_07/01_read.py
@@ -1,18 +1,25 @@
 # %%
-
+# atribui o arquivo de texto 'historia.txt' à variável 'nome_arquivo'
 nome_arquivo = "historia.txt"
+
+#%%
+# Abre arquivo em formato de leitura
+open_file = open(nome_arquivo)
+
+# Lê os dados do arquivo
+conteudo = open_file.read()
+print(conteudo)
+
+#%%
+# fecha o arquivo para garantir que ele possa ser alterado e não corrompa o próprio arquivo
+open_file.close()
+
+
+#%%
+
+# A forma mais usual para ler arquivos em Python
 
 with open(nome_arquivo) as open_file:
     conteudo = open_file.read()
 
 print(conteudo)
-
-# # Abre arquivo em formato de leitura
-# open_file = open(nome_arquivo)
-
-# # Lê os dados do arquivo
-# conteudo = open_file.read()
-# print(conteudo)
-
-# # fecha o arquivo
-# open_file.close()
